f78aac812fa08d36c850f67d52f9c3dfe0d1e855
Andrew <thum14252@gmail.com>
2024-04-14 08:58:30 +0630

    wrte and read

--- lession2/index.js
+++ lession2/index.js
@@ -1,12 +1,21 @@
 const { log } = require("console");
 const fs = require("fs");
+const path = require("path");
 
-fs.readFile('./text/start.txt', (err , data) => {
+fs.readFile(path.join(__dirname, "text" , 'starter.txt'), 'utf-8',(err , data) => {
     if(err) throw err;
     console.log(data); //Buffer data
     console.log(data.toString()); //string, UTF8 = string
 })
 
+
+fs.writeFile(path.join(__dirname, "text" , 'reply.txt'), "Nice To Meet You, Im from Wrte",(err) => {
+    if(err) throw err;
+    console.log("Write Complete");
+})
+
+
+console.log("hello")
 process.on("uncaughtException" , err => {
     console.error(`There is an Error : ${err}`);
     process.exit(1)
