31763636560a895d68fbb0d77413ffdbf763a8ba
Andrew <thum14252@gmail.com>
2024-04-16 16:13:22 +0630

    middle custome error handler

--- lession7/sever.js
+++ lession7/sever.js
@@ -8,14 +8,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const errorHandler = require('./middleware/errorHandler');
 const {logger } = require("./middleware/logEvent");
 const PORT = process.env.PORT || 3500;
 
 
 //custome middleware logger
-
 app.use(logger);
 
+//corss origin Res Sharing
+const whtieList = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whtieList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 
 //1-Built-In middleware tihandle urlencoded data
@@ -69,8 +83,17 @@ const three =( req , res ) =>{
 
 app.get("/chain(.html)?" , [one, two , three]);
 
-app.get("/*" , (req  ,res) =>{
-    res.status(404).sendFile(path.join(__dirname , "views" , "404.html"))
-})
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
 
 app.listen(PORT , () => console.log(`SEVER RUNNING ON PORT ${PORT}`));
