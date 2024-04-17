f82521fdede878b8a5ec5e2187b7e078f2d1ffd7
Andrew <thum14252@gmail.com>
2024-04-17 10:29:09 +0630

    Routing

--- lession8/sever.js
+++ lession8/sever.js
@@ -41,47 +41,13 @@ app.use(express.json());
 
 //sever static files
 app.use(express.static(path.join(__dirname, "/public")))
+app.use("/subdir",express.static(path.join(__dirname, "/public")))
 
+//routes
+app.use("/subdir" , require("./routes/subdir"));
+app.use("/", require("./routes/root"));
+app.use("/employees", require("./routes/api/employees"));
 
-
-
-app.get("^/$|/index(.html)?" , (req,  res) => {
-    // res.sendFile("./views/index.html" , {root: __dirname});
-    res.sendFile(path.join(__dirname, "views" , "index.html"));
-});
-app.get("/new-page(.html)?" , (req,  res) => {
-    // res.sendFile("./views/new-page.html" , {root: __dirname});
-    res.sendFile(path.join(__dirname, "views" , "new-page.html"));
-});
-app.get("/old-page(.html)?" , (req,  res) => {
-    // res.sendFile("./views/new-page.html" , {root: __dirname});
-    res.redirect(301,"/new-page.html"); //302 by default
-});
-
-//Route Handlers
-app.get("/hello(.html)?" ,(req , res , next) =>{
-    console.log("Attepmted to load hello.hmtl");
-    next();
-}, (req , res) => {
-    res.send("Hello Word!");
-})
-
-//changing route handlers
-
-const one =( req , res , next) =>{
-    console.log("one");
-    next();
-}
-const two =( req , res , next) =>{
-    console.log("two");
-    next();
-}
-const three =( req , res ) =>{
-    console.log("three");
-    res.send("Finished!")
-}
-
-app.get("/chain(.html)?" , [one, two , three]);
 
 app.all('*', (req, res) => {
     res.status(404);
