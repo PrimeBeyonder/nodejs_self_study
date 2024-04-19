0becc3085c49c44ce8071d82ddbf4e89abac9e42
Andrew <thum14252@gmail.com>
2024-04-19 11:32:12 +0630

    mongodb update

--- lession13/config/dbConn.js
+++ lession13/config/dbConn.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect("mongodb+srv://mongoDBTestUser:ANDREW123@cluster0.htirleh.mongodb.net/CompanyDB?retryWrites=true&w=majority&appName=Cluster0", {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
+         
+           
         });
     } catch (err) {
         console.error(err);
